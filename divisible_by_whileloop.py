0bfeb0880a3422dab09806d0385d80e29439091f
eyal-golan-gluck <113503780+eyal-golan-gluck@users.noreply.github.com>
2025-04-24 12:33:13 +0000

    While loop implementation of divisible numbers task

--- divisible_by_whileloop.py
+++ divisible_by_whileloop.py
@@ -1,1 +1,8 @@
-
+v_div = int(input("Please enter a divisor for detecting no-remainder division numbers: "))
+v_start = int(input("Please enter range start num: "))
+v_end = int(input("Please enter range end num: "))
+v_itr = v_start # Per while loop, initiation of var is necessary
+while v_itr in range(v_start, v_end):
+    if v_itr%v_div == 0:
+        print(f"{v_itr} is divisible by {v_div}")
+    v_itr += 1
