87df8d883cfb7f1c4e44f30c21d4797e8e546f04
eyal-golan-gluck <113503780+eyal-golan-gluck@users.noreply.github.com>
2025-04-24 08:54:08 +0000

    Odd/even logic added.

--- even_odd.py
+++ even_odd.py
@@ -3,7 +3,10 @@ while v_check.lower() != "exit":
     v_check = input("Please enter either a number to check parity, or just \"exit\" to quit. ")
     if v_check.isnumeric():
         v_check = int(v_check)
-        print(v_check)
+        if v_check%2 == 0:
+            print(f"{v_check} is even.")
+        else:
+            print(f"{v_check} is odd.")
         v_check = str(v_check)
     else:
         if v_check.lower() == "exit":
