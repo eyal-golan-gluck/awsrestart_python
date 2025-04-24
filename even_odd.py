4ccc58268240a01c7c59bccf3900c66ba1759840
eyal-golan-gluck <113503780+eyal-golan-gluck@users.noreply.github.com>
2025-04-24 08:51:30 +0000

    Odd or Even basic logic before functionality

--- even_odd.py
+++ even_odd.py
@@ -1,1 +1,11 @@
-
+v_check = ""
+while v_check.lower() != "exit":
+    v_check = input("Please enter either a number to check parity, or just \"exit\" to quit. ")
+    if v_check.isnumeric():
+        v_check = int(v_check)
+        print(v_check)
+        v_check = str(v_check)
+    else:
+        if v_check.lower() == "exit":
+            print("Have a nice day.")
+            break
