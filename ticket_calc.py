f07055283d3b908010c4edcf5c8205815a0efe30
eyal-golan-gluck <113503780+eyal-golan-gluck@users.noreply.github.com>
2025-04-25 07:14:58 +0000

    Releasing commit for further code to go in

--- ticket_calc.py
+++ ticket_calc.py
@@ -1,1 +1,7 @@
-
+from datetime import datetime
+v_age = ""
+while not(v_age.isnumeric()):
+    v_age = input("Enter your year of birth: ")
+v_loyalty = input("Loyalty member (y/n)? ")
+v_day = datetime.now().weekday
+print(f"{v_day}")
